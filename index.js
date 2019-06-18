b876f9f8ddb00cfa3b660fbf4ed66d779cde5845
alexandra12345678 <51681534+alexandra12345678@users.noreply.github.com>
2019-06-18 20:31:54 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-alert ("туст");
+function hamburgerClick() {
+    var mobileMenu = document.getElementsByClassName("mobile-menu")[0];
+  var display =  mobileMenu.style.display ;
+  if (display==""){
+      mobileMenu.style.display = "flex"; 
+    }  else {
+          mobileMenu.style.display ="";
+
+      }
+
+}
+function moreClick(event) {
+    event.preventDefault();
+
+    var itemWindow = document.getElementsByClassName("item-window")[0];
+    var display =  itemWindow.style.display ;
+  if (display==""){
+    itemWindow.style.display = "flex"; 
+    }  else {
+        itemWindow.style.display ="";
+}
+}
+var hamburgerIcon = document.getElementsByClassName("menu-icon")[0];
+hamburgerIcon.addEventListener("click", hamburgerClick);
+
+var moreIcon = document.getElementsByClassName("card-button")[1];
+moreIcon.addEventListener("click", moreClick);
+
+var closeIcon = document.getElementsByClassName("item-close")[0];
+closeIcon.addEventListener("click", moreClick);
